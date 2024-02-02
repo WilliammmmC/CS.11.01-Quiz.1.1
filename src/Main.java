72c3d9405cf6df17a045d2cf66870f83ad0e1a4c
WilliammmmC <157076893+WilliammmmC@users.noreply.github.com>
2024-02-02 13:56:29 +0800

    Update Main.java
    Unit Test

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        int age;
+        String firstName;
+        String favouriteFood;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");
+        age = scanner.nextInt();
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Please enter your first name:");
+        firstName = scanner1.nextLine();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Please enter your favourite food here:");
+        favouriteFood = scanner2.nextLine();
+        System.out.println("First Name:" + firstName + "\nAge:" + age + "\nFavourite food:" + favouriteFood);
     }
-
 }
+        
